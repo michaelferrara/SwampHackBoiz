0d03fc692945cefdf864699fd3642a97a0c7e3e8
michaelferrara <michael.ferrara@knights.ucf.edu>
2019-01-20 04:53:19 -0500

    Working Game

---
+++ js/main.js
@@ -0,0 +1,49 @@
+var sliders = $("#sliders .slider");
+
+sliders.each(function() {
+    var value = parseInt($(this).text(), 10),
+        availableTotal = 100;
+
+    $(this).empty().slider({
+        value: 0,
+        min: 0,
+        max: 100,
+        range: "max",
+        step: 1,
+        animate: 100,
+        slide: function(event, ui) {
+            // Update display to current value
+            $(this).siblings().text(ui.value + '%');
+
+            var className = $(this).attr('id');
+            className = "." + className;
+
+            var num = 60000*parseInt(ui.value)*.01;
+
+            $(className).text(num);
+
+            // Get current total
+            var total = 0;
+
+            sliders.not(this).each(function() {
+                total += $(this).slider("option", "value");
+            });
+
+            // Need to do this because apparently jQ UI
+            // does not update value until this event completes
+            total += ui.value;
+
+            var max = availableTotal - total;
+
+            // Update each slider
+            sliders.not(this).each(function() {
+                var t = $(this),
+                    value = t.slider("option", "value");
+
+                t.slider("option", "max", max + value)
+                    .siblings().text(value + '/' + (max + value) + '%');
+                t.slider('value', value);
+            });
+        }
+    });
+});
